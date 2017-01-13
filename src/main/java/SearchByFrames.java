0e95ad730c7166b61dc0f0b19491fa11fe19a589
Nikita Salomatin <nikita_salomatin@epam.com>
2017-01-14 01:54:32 +0400

    Initial commit

---
+++ src/main/java/SearchByFrames.java
@@ -0,0 +1,145 @@
+import com.codeborne.selenide.*;
+import com.codeborne.selenide.ex.UIAssertionError;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static org.openqa.selenium.support.ui.ExpectedConditions.frameToBeAvailableAndSwitchToIt;
+
+/**
+ * Created by Cok on 13.01.2017.
+ */
+public class SearchByFrames {
+
+    private final WebDriver driver;
+    private final Supplier<SelenideElement> supplier;
+
+    public static SearchByFrames of(By locator) {
+        return new SearchByFrames(locator);
+    }
+    public static SearchByFrames of(Supplier<SelenideElement> supplier) {
+        return new SearchByFrames(supplier);
+    }
+
+    private SearchByFrames(By locator) {
+        this(() -> $(locator));
+    }
+
+    private SearchByFrames(Supplier<SelenideElement> supplier) {
+        this.driver = WebDriverRunner.getWebDriver();
+        this.supplier = supplier;
+    }
+
+    public Optional<SelenideElement> getElem() {
+        long savedTimeout = Configuration.timeout;
+        long savedCollectionTimeout = Configuration.collectionsTimeout;
+        try {
+
+            Configuration.timeout = 200;
+            Configuration.collectionsTimeout = 200;
+
+            return findElementByFrames();
+        } catch (UIAssertionError error) {
+            return Optional.empty();
+        } finally {
+            Configuration.timeout = savedTimeout;
+            Configuration.collectionsTimeout = savedCollectionTimeout;
+        }
+    }
+
+    private Optional<SelenideElement> findElementByFrames() {
+
+        switchToTop();
+        List<SelenideElement> frames = findFrames();
+
+        if (isPresent(frames)) {
+            return lookThroughFrames(frames);
+        } else {
+            return Optional.empty();
+        }
+
+    }
+
+    private Optional<SelenideElement> lookThroughFrames(List<SelenideElement> frames) {
+
+        for (int i = 0; i < frames.size(); i++) {
+            if (i != 0) {
+                driver.switchTo().parentFrame();
+            }
+
+            if (switchToFrame(frames.get(i))) {
+
+                Optional<SelenideElement> result = lookElem();
+                if (result.isPresent()) {
+                    return result;
+                } else {
+                    List<SelenideElement> childFrames = findFrames();
+                    if (isPresent(childFrames)) {
+                        Optional<SelenideElement> selenideElement = lookThroughFrames(childFrames);
+                        if (selenideElement.isPresent()) {
+                             return selenideElement;
+                         }
+                    }
+                }
+            } else {
+                return Optional.empty();
+            }
+        }
+
+        driver.switchTo().parentFrame();
+        return Optional.empty();
+
+    }
+
+    private Optional<SelenideElement> lookElem() {
+
+            SelenideElement element = supplier.get();
+            if(element.is(Condition.exist)) {
+                return Optional.of(element);
+            } else {
+                return Optional.empty();
+            }
+    }
+
+    private boolean switchToFrame(SelenideElement selenideElement) {
+        try {
+            new FluentWait<>(driver)
+                    .withTimeout(300, TimeUnit.MILLISECONDS)
+                    .pollingEvery(100, TimeUnit.MILLISECONDS)
+                    .until(frameToBeAvailableAndSwitchToIt(selenideElement));
+            return true;
+        } catch (TimeoutException timeout) {
+            return false;
+        }
+    }
+
+    private boolean isPresent(List<SelenideElement> frames) {
+        return frames.size() > 0;
+    }
+
+    private List<SelenideElement> findFrames() {
+        try {
+           return  $$(driver.findElements(By.tagName("iframe")));
+        } catch (UIAssertionError error) {
+            return new ArrayList<>();
+        }
+//       return currentFrame.$$(By.tagName("iframe"));
+//        List<WebElement> frames = currentFrame.findElements(By.tagName("iframe"));
+//        return frames.stream().map(Selenide::$)
+//                .peek(WebElement::getSize)
+//                .collect(Collectors.toList());
+    }
+
+    private void switchToTop() {
+        driver.switchTo().defaultContent();
+    }
+}
---
+++ src/test/java/SearchFramesTest.java
@@ -0,0 +1,136 @@
+
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeTrue;
+
+/**
+ * Created by Cok on 13.01.2017.
+ */
+public class SearchFramesTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        Path sampleFile = Paths.get("src/test/resources/html/0001.html");
+        WebDriverRunner.getWebDriver().get(sampleFile.toUri().toString());
+        this.driver = WebDriverRunner.getWebDriver();
+
+    }
+
+    @Test
+    public void searchShouldReturnNotEmptyOptionalIfElementWasFound() throws Exception {
+
+        By locator = By.xpath(".//input[@name='firstname']");
+        SearchByFrames searchInFrame = SearchByFrames.of(locator);
+        Optional<SelenideElement> elem = searchInFrame.getElem();
+        assertTrue("Elem was found, but optional was an empty", elem.isPresent());
+
+    }
+
+    @Test
+    public void searchShouldReturnEmptyOptionalWhenElemWasNotFound() throws Exception {
+
+        By locator = By.xpath(".//input[@name='firstname1']");
+        SearchByFrames searchInFrame = SearchByFrames.of(locator);
+        Optional<SelenideElement> elem = searchInFrame.getElem();
+        assertFalse("Elem was not found, but optional was not empty", elem.isPresent());
+    }
+
+    @Test
+    public void elementShouldBeFoundInFrame() throws Exception {
+
+        By locator = By.xpath(".//input[@name='firstname']");
+        SearchByFrames searchInFrame = SearchByFrames.of(locator);
+        Optional<SelenideElement> elem = searchInFrame.getElem();
+        if (elem.isPresent()) {
+            SelenideElement selenideElement = elem.get();
+            boolean isPresent = selenideElement.is(exist);
+
+            assertTrue("Element was not found", isPresent);
+        } else {
+            assertTrue("Optional was empty, but should not be", elem.isPresent());
+        }
+
+    }
+
+
+    @Test
+    public void elementShouldBeFoundInFrameBySupplier() throws Exception {
+
+        By locator = By.xpath(".//input[@name='firstname']");
+        SearchByFrames searchInFrame = SearchByFrames.of(() -> $(By.tagName("body")).$(locator));
+        Optional<SelenideElement> elem = searchInFrame.getElem();
+        if (elem.isPresent()) {
+            SelenideElement selenideElement = elem.get();
+            boolean isPresent = selenideElement.is(exist);
+
+            assertTrue("Element was not found", isPresent);
+        } else {
+            assertTrue("Optional was empty, but should not be", elem.isPresent());
+        }
+
+    }
+
+    @Test
+    public void elementShouldBeFoundInFrameInsideFrame() throws Exception {
+
+        By locator = By.xpath(".//input[@name='firstname_child_1']");
+        SearchByFrames searchInFrame = SearchByFrames.of(locator);
+
+        try {
+            SelenideElement selenideElement = searchInFrame.getElem().get();
+            boolean isPresent = selenideElement.is(exist);
+            assertTrue("Element was not found", isPresent);
+        } catch (NoSuchElementException noElem) {
+            fail("Optional was empty, but should not be empty");
+        }
+    }
+
+    @Test
+    public void elementShouldBeFoundInDeepTree() throws Exception {
+
+        By locator = By.xpath(".//input[@name='firstname_child_deep_tree']");
+        SearchByFrames searchInFrame = SearchByFrames.of(locator);
+
+        try {
+            SelenideElement selenideElement = searchInFrame.getElem().get();
+            boolean isPresent = selenideElement.is(exist);
+            assertTrue("Element was not found", isPresent);
+        } catch (NoSuchElementException noElem) {
+            fail("Optional was empty, but should not be empty");
+        }
+
+    }
+
+    @Test
+    public void elementShouldBeFoundInLastFrameInTree() throws Exception {
+
+        By locator = By.xpath(".//input[@name='firstname_framesource_2']");
+        SearchByFrames searchInFrame = SearchByFrames.of(locator);
+        Optional<SelenideElement> elem = searchInFrame.getElem();
+        if (elem.isPresent()) {
+            SelenideElement selenideElement = elem.get();
+            boolean isPresent = selenideElement.is(exist);
+
+            assertTrue("Element was not found", isPresent);
+        } else {
+            assertTrue("Optional was empty, but should not be", elem.isPresent());
+        }
+    }
+}
